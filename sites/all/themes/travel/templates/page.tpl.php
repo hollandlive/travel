ebf23fe0ff54e189c7f9faa2de7ab72b00bd64d7
Artem Aksenov <hollandlive@gmail.com>
2016-11-28 14:03:14 +0100

    styling

--- sites/all/themes/travel/templates/page.tpl.php
+++ sites/all/themes/travel/templates/page.tpl.php
@@ -107,7 +107,7 @@
           <?php endif; ?>
           <?php if (!empty($page['navigation'])): ?>
             <?php print render($page['navigation']); ?>
-          <?php endif; ?> 
+          <?php endif; ?>
         </nav>
       </div>
     <?php endif; ?>
@@ -118,26 +118,22 @@
 
   <header role="banner" id="page-header">
     <?php if (!empty($site_slogan)): ?>
-      
-      
-      
       <p class="lead"><?php print $site_slogan; ?></p>
     <?php endif; ?>
 
     <?php print render($page['header']); ?>
   </header> <!-- /#page-header -->
-  
- <div class="row"><?php print render($page['jumbotron']); ?></div>
 
+  <div class="row"><?php print render($page['jumbotron']); ?></div>
   
+  <div class="row">
 
     <?php if (!empty($page['sidebar_first'])): ?>
       <aside class="col-sm-3" role="complementary">
         <?php print render($page['sidebar_first']); ?>
       </aside>  <!-- /#sidebar-first -->
-    <?php endif; ?>      
-      
-      
+    <?php endif; ?>
+
     <section<?php print $content_column_class; ?>>
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
@@ -146,10 +142,7 @@
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
-        
-
-        
-        
+        <h1 class="page-header"><?php print $title; ?></h1>
       <?php endif; ?>
       <?php print render($title_suffix); ?>
       <?php print $messages; ?>
@@ -162,13 +155,7 @@
       <?php if (!empty($action_links)): ?>
         <ul class="action-links"><?php print render($action_links); ?></ul>
       <?php endif; ?>
-        
-        
-        
-        <div class="row"><?php print render($page['system_main']['#block']->title); ?></div>
-     
-
- <?php print render($page['content']); ?>
+      <?php print render($page['content']); ?>
     </section>
 
     <?php if (!empty($page['sidebar_second'])): ?>
@@ -181,7 +168,9 @@
 </div>
 
 
-<div class="row"><?php print render($page['carousel']); ?></div>
+
+  <div class="row"><?php print render($page['carousel']); ?></div>
+
 
 
 
